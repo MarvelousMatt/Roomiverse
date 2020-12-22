be6d047a0a50562a3dddf7d04683fca42061bd02
MarvelousMatt <mattjenkinswork@gmail.com>
2020-12-22 16:52:49 +0000

    Added a better looking player model assembly as well as making the player rotate towards the direction that is placed. The rotation is not smooth but that can be fixed in the future.

---
+++ Assets/PlayerModelRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerModelRotation : MonoBehaviour
+{
+    Rigidbody r;
+    public GameObject model;
+    public float modelRotationSpeed;
+    public float modelRotationTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        float targetAngle = Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Mathf.Rad2Deg;
+
+        model.transform.up = transform.up;
+
+        model.transform.rotation *= Quaternion.Euler(new Vector3(0, targetAngle, 0));
+    }
+}
