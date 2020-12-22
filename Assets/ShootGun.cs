1f8996aa1f018d47f053269d81b845a201c7e54a
MarvelousMatt <mattjenkinswork@gmail.com>
2020-12-22 17:32:24 +0000

    Added some more materials for the player model and bullets. Added script for gunfire (as yet unused)

---
+++ Assets/ShootGun.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootGun : MonoBehaviour
+{
+    float projectileLifetime;
+    float projectileSpeed;
+    GameObject bulletSpawn;
+
+
+    public void Shoot()
+    {
+
+    }
+}
